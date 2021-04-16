fa7c88dabbc1a29755aa56178cb9659572ddbf8f
maromaroXD <amine-fb-fb@hotmail.fr>
2021-04-16 21:40:12 +0000

    drawing idle paddles

--- main.lua
+++ main.lua
@@ -16,12 +16,23 @@ local winOptions = {
 
 love.load = function()
 	love.graphics.setDefaultFilter('nearest', 'nearest');
+	local retroF = love.graphics.newFont('font.ttf', 8);
+	love.graphics.setFont(retroF);
 	push:setupScreen(VIRTUAL_WIDTH, VIRTUAL_HEIGHT,WINDOW_WIDTH, WINDOW_HEIGHT, winOptions);
 end
 
 love.draw = function()
 	push:apply('start');
-	love.graphics.printf('Hello',0 ,VIRTUAL_HEIGHT / 2 - 6, VIRTUAL_WIDTH, 'center' );
+	love.graphics.clear(40/255, 45/255, 52/255, 255/255);
+	love.graphics.printf('Hello',0 , 20, VIRTUAL_WIDTH, 'center' );
+
+	-- left paddle
+	love.graphics.rectangle('fill', 10, 30, 5, 20);
+
+	-- right paddle
+	love.graphics.rectangle('fill', VIRTUAL_WIDTH - 10, VIRTUAL_HEIGHT - 50, 5, 20);
+
+
 	push:apply('end');
 end
 
